ae13fc1fdb389573234016bd4a33b7e82e4ab288
Adam Brown <adam.edward.brown@gmail.com>
2020-08-18 14:34:21 -0400

    seed blessings adds emojis, does not truncate or anything

--- seeds/seed.blessings.sql
+++ seeds/seed.blessings.sql
@@ -1,12 +1,7 @@
 BEGIN;
-  TRUNCATE
-        blessings
-        RESTART IDENTITY CASCADE;
-
 INSERT INTO blessings
   (blessing)
 VALUES
-  ('U+1F91F'),
   ('U+1F607'),
   ('U+1F92C'),
   ('U+1F602'),
